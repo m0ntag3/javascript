f4082a5b1e46e6f7148c7ea5396358183a51d976
m0ntag3 <kuminacollins@gmail.com>
2026-02-25 15:17:36 +0300

    js assign

--- assignment1.js
+++ assignment1.js
@@ -1,4 +1,18 @@
 // Object data type
+// Data type equivalent to string but stores data in key-value pairs.
 
+const car_details = {
+    car_name : "Bolide",
+    brand : "Bugatti",
+    condition : "new",
+    top_speed : 380
+};
+console.log(car_details)
+console.log(typeof(car_details))
 
 // Array data type
+// data type with the ability to store other data types like strings, numbers and even other arrays.
+const computer_components = ["Desktop", "Monitor", "Mouse","Keyboard"];
+console.log(computer_components)
+console.log(typeof(computer_components))
+
---
+++ test.js
@@ -0,0 +1,11 @@
+const user = {
+  firstName: "Alice",    
+  age: 30,               
+  isPremium: true        
+};
+console.log(user)
+
+const colors = [];
+colors[0] = "Red";
+colors[1] = "Blue";
+console.log(colors)
