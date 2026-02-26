d0567f92aa767c9d9ed25cdce49f303a4c70c4d2
m0ntag3 <kuminacollins@gmail.com>
2026-02-26 14:11:38 +0300

    assignjs

---
+++ assignment2.js
@@ -0,0 +1,15 @@
+let distance =  7000
+
+if (distance <= 100){
+    console.log("Amount to pay is 5 USD")
+}
+else if (distance > 100 && distance <= 500){
+    console.log("Amount to Pay is 10 USD")
+}
+else if (distance > 500 && distance <= 1000){
+    console.log("Amount to Pay is 20 USD")
+}
+else {
+    console.log("Amount to Pay is 40 USD")
+}
+    
